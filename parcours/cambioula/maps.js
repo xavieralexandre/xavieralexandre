52aa7668ddbb00e3025a14d0be5929f5a7d492cb
Xavier Alexandre <xavier@Xaviers-Air.fritz.box>
2016-11-05 23:58:25 +0100

    Misc

--- parcours/cambioula/maps.js
+++ parcours/cambioula/maps.js
@@ -4,7 +4,7 @@ var map = new ga.Map({
   layers: [layer],
   interactions: ol.interaction.defaults({mouseWheelZoom: false, dragPan: false}),
   view: new ol.View({
-    resolution: 10,
+    resolution: 9,
     center: [598336, 114540]
   }),
 });
