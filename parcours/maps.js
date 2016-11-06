181926c00bb3903bda99573c949b14100ce1dd12
Xavier Alexandre <xavier@Xaviers-Air.fritz.box>
2016-11-06 22:01:37 +0100

    Misc

---
+++ parcours/maps.js
@@ -0,0 +1,64 @@
+var layer = ga.layer.create('ch.swisstopo.pixelkarte-grau');
+var map = new ga.Map({
+  target: 'map',
+  layers: [layer],
+  interactions: ol.interaction.defaults({mouseWheelZoom: false, dragPan: false}),
+  view: new ol.View({
+    resolution: 100,
+    center: [560000, 140000]
+  }),
+});
+
+// Create the KML Layer
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'http://xavieralexandre.org/parcours/lavey-morcles/lavey-morcles.kml',
+    format : new ol.format.KML({
+      projection: 'EPSG:21781'
+    }),
+    extractStyles: true
+  }),
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'rgba(255,0,0,0.8)',
+      width: 6
+    }),
+  })
+});
+map.addLayer(vector);
+
+// Create the KML Layer
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'http://xavieralexandre.org/parcours/cambioula/cambioula.kml',
+    format : new ol.format.KML({
+      projection: 'EPSG:21781'
+    }),
+    extractStyles: true
+  }),
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'rgba(255,0,0,0.8)',
+      width: 6
+    }),
+  })
+});
+map.addLayer(vector);
+
+// Create the KML Layer
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'http://xavieralexandre.org/parcours/derborence/derborence.kml',
+    format : new ol.format.KML({
+      projection: 'EPSG:21781'
+    }),
+    extractStyles: true
+  }),
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'rgba(255,0,0,0.8)',
+      width: 6
+    }),
+  })
+});
+map.addLayer(vector);
