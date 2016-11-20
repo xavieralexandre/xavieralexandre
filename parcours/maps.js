a63f8fc0ffc2a2fe10b4bd8f592449e374490add
Xavier Alexandre <xavier@Xaviers-Air.fritz.box>
2016-11-20 21:59:31 +0100

    Fresh

--- parcours/maps.js
+++ parcours/maps.js
@@ -1,82 +1,70 @@
+var bex = [570000, 137000];
+
+var view = new ol.View({
+  center: bex,
+  resolution: 100
+});
+
 var layer = ga.layer.create('ch.swisstopo.pixelkarte-grau');
+
 var map = new ga.Map({
-  target: 'map',
   layers: [layer],
-  interactions: ol.interaction.defaults({mouseWheelZoom: false, dragPan: false}),
-  view: new ol.View({
-    resolution: 100,
-    center: [560000, 140000]
-  }),
+  loadTilesWhileAnimating: true,
+  target: 'map',
+  view: view
 });
 
-// Create the KML Layer
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'http://xavieralexandre.org/parcours/lavey-morcles/lavey-morcles.kml',
-    format : new ol.format.KML({
-      projection: 'EPSG:21781'
-    }),
-    extractStyles: true
-  }),
-  style: new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgba(255,0,0,0.8)',
-      width: 6
-    }),
-  })
-});
-map.addLayer(vector);
+var map_track = function(kml) {
+  var source = new ol.source.Vector({
+    url: 'http://xavieralexandre.org/parcours/' + kml + '/' + kml + '.kml',
+    format: new ol.format.KML({})
+  });
 
-// Create the KML Layer
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'http://xavieralexandre.org/parcours/cambioula/cambioula.kml',
-    format : new ol.format.KML({
-      projection: 'EPSG:21781'
-    }),
-    extractStyles: true
-  }),
-  style: new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgba(255,0,0,0.8)',
-      width: 6
-    }),
-  })
-});
-map.addLayer(vector);
+  var vector = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: 'rgba(255,0,0,0.8)',
+        width: 2
+      }),
+    })
+  });
+  map.addLayer(vector);
 
-// Create the KML Layer
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'http://xavieralexandre.org/parcours/derborence/derborence.kml',
-    format : new ol.format.KML({
-      projection: 'EPSG:21781'
-    }),
-    extractStyles: true
-  }),
-  style: new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgba(255,0,0,0.8)',
-      width: 6
-    }),
-  })
-});
-map.addLayer(vector);
+  var zoom_to_track = function(kml) {
+    var zoomto = document.getElementById(kml);
+    zoomto.addEventListener('mouseenter', function() {
+      var duration = 1000;
+      var start = +new Date();
+      var pan = ol.animation.pan({
+        duration: duration,
+        source: map.getView().getCenter(),
+        start: start
+      });
+      var zoom = ol.animation.zoom({
+        duration: duration,
+        resolution: map.getView().getResolution(),
+        start: start
+      });
+      var bounce = ol.animation.bounce({
+        duration: duration,
+        resolution: 2 * map.getView().getResolution(),
+        start: start,
+        easing: ol.easing.upAndDown
+      });
 
-// Create the KML Layer
-var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    url: 'http://xavieralexandre.org/parcours/gruyeres/gruyeres.kml',
-    format : new ol.format.KML({
-      projection: 'EPSG:21781'
-    }),
-    extractStyles: true
-  }),
-  style: new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'rgba(255,0,0,0.8)',
-      width: 6
-    }),
-  })
-});
-map.addLayer(vector);
+      map.beforeRender(pan, zoom, bounce);
+      var extent = source.getExtent();
+      map.getView().fitExtent(extent, map.getSize());
+    }, false);
+  };
+  zoom_to_track(kml);
+};
+
+var allElements = document.getElementsByClassName('track');
+var allIds = [];
+for (var i = 0, n = allElements.length; i < n; ++i) {
+  var el = allElements[i];
+  if (el.id) { allIds.push(el.id); }
+  map_track(el.id)
+}
