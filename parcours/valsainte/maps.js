789f5c63f939486369ad58a106d6c7967db54df1
Xavier <cuocoli@gmail.com>
2019-05-16 07:19:38 +0200

    updated map

--- parcours/valsainte/maps.js
+++ parcours/valsainte/maps.js
@@ -12,7 +12,7 @@ var map = new ga.Map({
 // Create the KML Layer
 var vector = new ol.layer.Vector({
   source: new ol.source.Vector({
-    url: 'http://xavieralexandre.org/parcours/schwarzsee/schwarzsee.kml',
+    url: 'http://xavieralexandre.org/parcours/schwarzsee/valsainte.kml',
     format : new ol.format.KML({
       projection: 'EPSG:21781'
     }),
