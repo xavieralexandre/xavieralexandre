7242bce073ba40051e6e0c242d09d5a2d2b5de4e
Xavier <cuocoli@gmail.com>
2019-06-02 14:51:32 +0200

    Changes

--- fondue/README.md
+++
@@ -1,3 +0,0 @@
-http://duvindufromageetdupain.ch/
-
-Preneur de coup de main pour les ressources fondues en Lavaux et pour le code :)
--- fondue/phantom.js
+++
@@ -1,9 +0,0 @@
-var page = require('webpage').create();
-page.open('http://localhost:5757/map.html', function() {
-  page.clipRect = { top: 2, left: 2, width: 800, height: 800 };
-
-  window.setTimeout(function () {
-    page.render('map_preview.png');
-    phantom.exit();
-  }, 2000);
-});
